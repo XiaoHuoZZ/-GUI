a235f3c3812cf69411ff5c324f1c5c5be690fc6b
fx <314297421@qq.com>
2019-05-07 16:08:47 +0800

    基本功能实现

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -28,23 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.selectFile_btn = new System.Windows.Forms.Button();
+            this.selectPy_btn = new System.Windows.Forms.Button();
+            this.execute_btn = new System.Windows.Forms.Button();
+            this.filePath_tbox = new System.Windows.Forms.TextBox();
+            this.pyPath_tbox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // selectFile_btn
+            // 
+            this.selectFile_btn.Location = new System.Drawing.Point(73, 100);
+            this.selectFile_btn.Name = "selectFile_btn";
+            this.selectFile_btn.Size = new System.Drawing.Size(75, 23);
+            this.selectFile_btn.TabIndex = 0;
+            this.selectFile_btn.Text = "选择数据源";
+            this.selectFile_btn.UseVisualStyleBackColor = true;
+            this.selectFile_btn.Click += new System.EventHandler(this.SelectFile_btn_Click);
+            // 
+            // selectPy_btn
+            // 
+            this.selectPy_btn.Location = new System.Drawing.Point(73, 129);
+            this.selectPy_btn.Name = "selectPy_btn";
+            this.selectPy_btn.Size = new System.Drawing.Size(75, 23);
+            this.selectPy_btn.TabIndex = 1;
+            this.selectPy_btn.Text = "选择脚本";
+            this.selectPy_btn.UseVisualStyleBackColor = true;
+            this.selectPy_btn.Click += new System.EventHandler(this.SelectPy_btn_Click);
+            // 
+            // execute_btn
+            // 
+            this.execute_btn.Location = new System.Drawing.Point(196, 227);
+            this.execute_btn.Name = "execute_btn";
+            this.execute_btn.Size = new System.Drawing.Size(75, 23);
+            this.execute_btn.TabIndex = 2;
+            this.execute_btn.Text = "执行";
+            this.execute_btn.UseVisualStyleBackColor = true;
+            this.execute_btn.Click += new System.EventHandler(this.Execute_btn_Click);
+            // 
+            // filePath_tbox
+            // 
+            this.filePath_tbox.Location = new System.Drawing.Point(155, 101);
+            this.filePath_tbox.Name = "filePath_tbox";
+            this.filePath_tbox.Size = new System.Drawing.Size(229, 21);
+            this.filePath_tbox.TabIndex = 3;
+            // 
+            // pyPath_tbox
+            // 
+            this.pyPath_tbox.Location = new System.Drawing.Point(155, 131);
+            this.pyPath_tbox.Name = "pyPath_tbox";
+            this.pyPath_tbox.Size = new System.Drawing.Size(229, 21);
+            this.pyPath_tbox.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(530, 297);
+            this.Controls.Add(this.pyPath_tbox);
+            this.Controls.Add(this.filePath_tbox);
+            this.Controls.Add(this.execute_btn);
+            this.Controls.Add(this.selectPy_btn);
+            this.Controls.Add(this.selectFile_btn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "离群点分析";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button selectFile_btn;
+        private System.Windows.Forms.Button selectPy_btn;
+        private System.Windows.Forms.Button execute_btn;
+        private System.Windows.Forms.TextBox filePath_tbox;
+        private System.Windows.Forms.TextBox pyPath_tbox;
     }
 }
 
